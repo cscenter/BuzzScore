7e4e9619235fa20fcfa1afaaba21d17a49b9ef9c
Nikita Kartashov <snailandmail@gmail.com>
2013-11-02 17:54:37 +0400

    Added Twitter Bootstrap and changed base template to use it, added a custom css style for index page, removed jquery file replacing it with a call to web api, removed unused parameter from tweet search form

--- buzz_score_site/buzz_score/forms.py
+++ buzz_score_site/buzz_score/forms.py
@@ -5,7 +5,6 @@ from django import forms
 SUPPORTED_LANGUAGES = (('en', 'en'), ('ru', 'ru'))
 MAX_QUERY_LENGTH = 100
 MIN_QUERY_LENGTH = 3
-MAX_RESULTS = 1000
 
 
 class EmotionalEvaluationForm (forms.Form):
