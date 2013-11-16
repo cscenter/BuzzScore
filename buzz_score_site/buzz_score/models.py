07412966193571f4e01ca3f1f5f89cd137949f48
Nikita Kartashov <snailandmail@gmail.com>
2013-11-16 16:15:45 +0400

    Revert "Added a model class to present tweets in html, attributes for filtering and script for the same purpose"
    This reverts commit 08df5d014097c8a7249aef398bd7509ff5642b02.

--- buzz_score_site/buzz_score/models.py
+++ buzz_score_site/buzz_score/models.py
@@ -1,23 +1,3 @@
+from django.db import models
+
 # Create your models here.
-
-
-class Tweet:
-    _tweet_dict = {}
-
-    @staticmethod
-    def construct_tweet_collection(l):
-        return [Tweet(item) for item in l]
-
-    def __init__(self, tweet_dict):
-        self._tweet_dict = tweet_dict
-
-    def as_html(self):
-        output = '<div class="tweet"' \
-                 'favorited="%s" retweet_count="%s" is_retweet="%r">' \
-                 '<p> @%s tweeted %s </p>' \
-                 '</div>' % (self._tweet_dict['favorited'],
-                             self._tweet_dict['retweet_count'],
-                             'retweeted_status' in self._tweet_dict.keys(),
-                             self._tweet_dict['user']['screen_name'],
-                             self._tweet_dict['text'])
-        return output
